95ec896f007fb0cd84149cf2ab74e6a13f859274
DawidNieborak <dnieborak@consdata.com>
2023-08-18 20:41:19 +0200

    primeflex fix

--- multiplayer-team-fight/src/app/app-routing.module.ts
+++ multiplayer-team-fight/src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import {EndComponent} from "./end/end.component";
+import {StartComponent} from "./start/start.component";
 
 const routes: Routes = [
   { path: 'game-over', component: EndComponent },
+  { path: 'game-start', component: StartComponent },
   // ...
 ];
 @NgModule({
