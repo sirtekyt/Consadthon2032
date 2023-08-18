891df118d14539e1889c4e095f03df78a55edb52
DawidNieborak <dnieborak@consdata.com>
2023-08-18 20:17:45 +0200

    poprawki

--- multiplayer-team-fight/src/app/app.module.ts
+++ multiplayer-team-fight/src/app/app.module.ts
@@ -8,7 +8,6 @@ import { FormsModule } from '@angular/forms';
 import { StartComponent } from './start/start.component';
 import { EndComponent } from './end/end.component';
 import { GameComponent } from './game/game.component';
-import { HeaderComponent } from './header/header.component';
 import { ResultsComponent } from './results/results.component';
 
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
@@ -22,7 +21,6 @@ const config: SocketIoConfig = {url: 'http://localhost:3000', options: {}}
     StartComponent,
     EndComponent,
     GameComponent,
-    HeaderComponent,
     ResultsComponent
   ],
   imports: [
