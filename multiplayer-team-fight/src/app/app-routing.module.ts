1019259993dfde89874963739f5e05fd2233ece6
bdomzalski <bdomzalski@consdata.com>
2023-08-18 20:12:34 +0200

    routing module.

--- multiplayer-team-fight/src/app/app-routing.module.ts
+++ multiplayer-team-fight/src/app/app-routing.module.ts
@@ -1,8 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {EndComponent} from "./end/end.component";
 
-const routes: Routes = [];
-
+const routes: Routes = [
+  { path: 'game-over', component: EndComponent },
+  // ...
+];
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
--- multiplayer-team-fight/src/app/end/end.component.ts
+++ multiplayer-team-fight/src/app/end/end.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {Router} from "@angular/router";
 
 @Component({
   selector: 'app-end',
@@ -6,5 +7,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./end.component.css']
 })
 export class EndComponent {
+  constructor(private router:Router) {
+  }
+  restartGame(){
 
+  }
 }
